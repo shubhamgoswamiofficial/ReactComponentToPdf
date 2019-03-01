2209c88c3226928d2479375ec675f72990e00cf0
shubhamgoswami-official <shubham.goswami@mobcoder.com>
2019-03-01 16:33:23 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React, { Component } from 'react';
 import { PDFExport } from '@progress/kendo-react-pdf';
-import { faGithub, faMedium } from '@fortawesome/free-brands-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import canvg from 'canvg';
-import ReactDOMServer from 'react-dom/server';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import FormControl from '@material-ui/core/FormControl';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import withStyles from '@material-ui/core/styles/withStyles';
+// import { faGithub, faMedium } from '@fortawesome/free-brands-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import canvg from 'canvg';
+// import ReactDOMServer from 'react-dom/server';
+
+import {Button,CssBaseline,FormControl,FormControlLabel, Checkbox, Input, InputLabel, Paper, Typography,withStyles } from '@material-ui/core';
 import PropTypes from 'prop-types';
 
 class App extends Component {
